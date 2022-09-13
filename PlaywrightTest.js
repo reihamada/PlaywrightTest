3ee317d3e29a3fac6b789d5f64d9e3f258a3b1a7
Rei Hamada <hamada.rei@nci.co.jp>
2022-09-13 17:40:14 +0900

    とりま

--- PlaywrightTest.js
+++ PlaywrightTest.js
@@ -1,7 +1,7 @@
 const pw = require('playwright');
 
 (async () => {
-  const browser = await pw.webkit.launch(); // or 'chromium', 'firefox'
+  const browser = await pw.webkit.launch({ headless: false }); // or 'chromium', 'firefox'
   const context = await browser.newContext();
   const page = await context.newPage();
 
@@ -11,17 +11,17 @@ const pw = require('playwright');
   await page.screenshot({ path: 'example1.png' });
   await page.type('#username', 'hamada.rei@nci.co.jp'); // ユーザー名入力
   await page.type('#password', 'testtesttest'); // パスワード入力
+//   await page.waitForSelector('amplify-form-section > form > amplify-section > section > div:nth-child(4) > div > slot > div > slot > amplify-button > button');
   await page.screenshot({ path: 'example2.png' });
-  page.click(
+  await page.click(
     'amplify-form-section > form > amplify-section > section > div:nth-child(4) > div > slot > div > slot > amplify-button > button'
   );
 
-  await page.waitForSelector('#app > div > h1');
+  await page.waitForSelector('#app > h1');
   await page.screenshot({ path: 'example3.png' });
 
   await page.waitForSelector('amplify-button > button');
-  page.click('amplify-button > button');
-  await page.screenshot({ path: 'example4.png' });
+  await page.click('amplify-button > button');
 
   await browser.close();
 })();
